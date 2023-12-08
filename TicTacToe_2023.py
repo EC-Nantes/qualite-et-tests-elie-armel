9b3d76333bc8e85e2ca37e30e684609c8aeaf149
armelesper <armel.esper@eleves.ec-nantes.fr>
2023-12-08 15:31:29 +0100

    Changes to be committed: 	modified:   TicTacToe_2023.py

--- TicTacToe_2023.py
+++ TicTacToe_2023.py
@@ -1,59 +1,197 @@
-def print_board(board):
-    for row in board:
-        print(" | ".join(row))
-        print("-" * 5)
+# Pam Qian 2016 Fall CS 112 Python Midterm Project II
+# Tic Tack Toe 
 
-def check_winner(board, player):
-    # Check rows and columns
-    for i in range(3):
-        if all(board[i][j] == player for j in range(3)) or all(board[j][i] == player for j in range(3)):
-            return True
+def main():
+# The main function
+    introduction = intro()
+    board = create_grid()
+    pretty = printPretty(board)
+    symbol_1, symbol_2 = sym()
+    full = isFull(board, symbol_1, symbol_2) # The function that starts the game is also in here.
+    
 
-    # Check diagonals
-    if all(board[i][i] == player for i in range(3)) or all(board[i][2 - i] == player for i in range(3)):
-        return True
+    
 
-    return False
 
-def is_board_full(board):
-    return all(board[i][j] != ' ' for i in range(3) for j in range(3))
+def intro():
+# This function introduces the rules of the game Tic Tac Toe
+    print("Hello! Welcome to Pam's Tic Tac Toe game!")
+    print("\n")
+    print("Rules: Player 1 and player 2, represented by X and O, take turns "
+          "marking the spaces in a 3*3 grid. The player who succeeds in placing "
+          "three of their marks in a horizontal, vertical, or diagonal row wins.")
+    print("\n")
+    input("Press enter to continue.")
+    print("\n")
 
-def play_tic_tac_toe():
-    board = [[' ' for _ in range(3)] for _ in range(3)]
-    current_player = 'X'
 
-    while True:
-        print_board(board)
 
-        # Get player move
-        while True:
-            try:
-                row = int(input("Enter row (0, 1, or 2): "))
-                col = int(input("Enter column (0, 1, or 2): "))
-                if 0 <= row < 3 and 0 <= col < 3 and board[row][col] == ' ':
-                    break
-                else:
-                    print("Invalid move. Try again.")
-            except ValueError:
-                print("Invalid input. Please enter a number.")
+def create_grid():
+# This function creates a blank playboard
+    print("Here is the playboard: ")
+    board = [[" ", " ", " "],
+             [" ", " ", " "],
+             [" ", " ", " "]]        
+    return board
 
-        # Make the move
-        board[row][col] = current_player
 
-        # Check for a winner
-        if check_winner(board, current_player):
-            print_board(board)
-            print(f"Player {current_player} wins!")
-            break
 
-        # Check for a tie
-        if is_board_full(board):
-            print_board(board)
-            print("It's a tie!")
-            break
+def sym():
+# This function decides the players' symbols
+    symbol_1 = input("Player 1, do you want to be X or O? ")
+    if symbol_1 == "X":
+        symbol_2 = "O"
+        print("Player 2, you are O. ")
+    else:
+        symbol_2 = "X"
+        print("Player 2, you are X. ")
+    input("Press enter to continue.")
+    print("\n")
+    return (symbol_1, symbol_2)
 
-        # Switch to the other player
-        current_player = 'O' if current_player == 'X' else 'X'
 
-if __name__ == "__main__":
-    play_tic_tac_toe()
+
+def startGamming(board, symbol_1, symbol_2, count):
+# This function starts the game.
+
+    # Decides the turn
+    if count % 2 == 0:
+        player = symbol_1
+    elif count % 2 == 1:
+        player = symbol_2
+    print("Player "+ player + ", it is your turn. ")
+    row = int(input("Pick a row:"
+                    "[upper row: enter 0, middle row: enter 1, bottom row: enter 2]:"))
+    column = int(input("Pick a column:"
+                       "[left column: enter 0, middle column: enter 1, right column enter 2]"))
+
+
+    # Check if players' selection is out of range
+    while (row > 2 or row < 0) or (column > 2 or column < 0):
+        outOfBoard(row, column)
+        row = int(input("Pick a row[upper row:"
+                        "[enter 0, middle row: enter 1, bottom row: enter 2]:"))
+        column = int(input("Pick a column:"
+                           "[left column: enter 0, middle column: enter 1, right column enter 2]"))
+
+        # Check if the square is already filled
+    while (board[row][column] == symbol_1)or (board[row][column] == symbol_2):
+        filled = illegal(board, symbol_1, symbol_2, row, column)
+        row = int(input("Pick a row[upper row:"
+                        "[enter 0, middle row: enter 1, bottom row: enter 2]:"))
+        column = int(input("Pick a column:"
+                            "[left column: enter 0, middle column: enter 1, right column enter 2]"))    
+        
+    # Locates player's symbol on the board
+    if player == symbol_1:
+        board[row][column] = symbol_1
+            
+    else:
+        board[row][column] = symbol_2
+    
+    return (board)
+
+
+
+def isFull(board, symbol_1, symbol_2):
+    count = 1
+    winner = True
+# This function check if the board is full
+    while count < 10 and winner == True:
+        gaming = startGamming(board, symbol_1, symbol_2, count)
+        pretty = printPretty(board)
+        
+        if count == 9:
+            print("The board is full. Game over.")
+            if winner == True:
+                print("There is a tie. ")
+
+        # Check if here is a winner
+        winner = isWinner(board, symbol_1, symbol_2, count)
+        count += 1
+    if winner == False:
+        print("Game over.")
+        
+    # This is function gives a report 
+    report(count, winner, symbol_1, symbol_2)
+
+
+
+def outOfBoard(row, column):
+# This function tells the players that their selection is out of range
+    print("Out of boarder. Pick another one. ")
+    
+    
+
+def printPretty(board):
+# This function prints the board nice!
+    rows = len(board)
+    cols = len(board)
+    print("---+---+---")
+    for r in range(rows):
+        print(board[r][0], " |", board[r][1], "|", board[r][2])
+        print("---+---+---")
+    return board
+
+
+
+def isWinner(board, symbol_1, symbol_2, count):
+# This function checks if any winner is winning
+    winner = True
+    # Check the rows
+    for row in range (0, 3):
+        if (board[row][0] == board[row][1] == board[row][2] == symbol_1):
+            winner = False
+            print("Player " + symbol_1 + ", you won!")
+   
+        elif (board[row][0] == board[row][1] == board[row][2] == symbol_2):
+            winner = False
+            print("Player " + symbol_2 + ", you won!")
+            
+            
+    # Check the columns
+    for col in range (0, 3):
+        if (board[0][col] == board[1][col] == board[2][col] == symbol_1):
+            winner = False
+            print("Player " + symbol_1 + ", you won!")
+        elif (board[0][col] == board[1][col] == board[2][col] == symbol_2):
+            winner = False
+            print("Player " + symbol_2 + ", you won!")
+
+    # Check the diagnoals
+    if board[0][0] == board[1][1] == board[2][2] == symbol_1:
+        winner = False 
+        print("Player " + symbol_1 + ", you won!")
+
+    elif board[0][0] == board[1][1] == board[2][2] == symbol_2:
+        winner = False
+        print("Player " + symbol_2 + ", you won!")
+
+    elif board[0][2] == board[1][1] == board[2][0] == symbol_1:
+        winner = False
+        print("Player " + symbol_1 + ", you won!")
+
+    elif board[0][2] == board[1][1] == board[2][0] == symbol_2:
+        winner = False
+        print("Player " + symbol_2 + ", you won!")
+
+    return winner
+    
+
+
+def illegal(board, symbol_1, symbol_2, row, column):
+    print("The square you picked is already filled. Pick another one.")
+
+    
+def report(count, winner, symbol_1, symbol_2):
+    print("\n")
+    input("Press enter to see the game summary. ")
+    if (winner == False) and (count % 2 == 1 ):
+        print("Winner : Player " + symbol_1 + ".")
+    elif (winner == False) and (count % 2 == 0 ):
+        print("Winner : Player " + symbol_2 + ".")
+    else:
+        print("There is a tie. ")
+
+# Call Main
+main()
